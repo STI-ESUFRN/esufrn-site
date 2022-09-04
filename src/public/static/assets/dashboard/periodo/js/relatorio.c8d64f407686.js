2202e63e80630b5a17593ba58510e7867bba7531
Suporte <suporteeen@gmail.com>
2022-09-04 02:28:11 -0300

    rcleaned repository

--- src/public/static/assets/dashboard/periodo/js/relatorio.c8d64f407686.js
+++
@@ -1,9 +0,0 @@
-
-$("[data-filter]").change(function () {
-    filters = $("[data-filter]").serialize();
-    window.location.replace(window.location.href.replace(/[\?#].*|$/, `?${filters}`));
-});
-
-function backTop() {
-    $('html, body').animate({ scrollTop: '0px' }, 300);
-};
