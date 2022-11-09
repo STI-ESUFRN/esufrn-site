33445f73cfc032eeb1656a79d82f186b3012ed7a
Suporte <suporteeen@gmail.com>
2022-11-09 14:58:42 -0300

    fixing pre-commit

--- src/revistas/apps.py
+++ src/revistas/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class RevistasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'revistas'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "revistas"
