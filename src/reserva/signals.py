8ff4af0cf1b76049bfe00299c6e3bfcc954a7ec5
Felipe Sena <felipesena.m@gmail.com>
2023-02-12 20:34:32 -0300

    adding link to cancel reserve in confirmation email

--- src/reserva/signals.py
+++ src/reserva/signals.py
@@ -1,8 +1,14 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from reserva.helpers import notify_admin, notify_done, notify_requester
 from reserva.models import Reserve
+
+
+@receiver(pre_save, sender=Reserve)
+def notify_reserve_done(sender, instance, **kwargs):
+    if not instance.pk or instance.status != Reserve.objects.get(id=instance.id).status:
+        notify_done(instance)
 
 
 @receiver(post_save, sender=Reserve)
@@ -10,6 +16,3 @@ def notify_reserve(sender, instance, created, **kwargs):
     if created:
         notify_admin(instance)
         notify_requester(instance)
-
-    elif instance.status != Reserve.objects.get(id=instance.id).status:
-        notify_done(instance)
--- src/reserva/urls.py
+++ src/reserva/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from reserva.views import home_reserva
+from reserva.views import cancel_reserve, home_reserva
 
 urlpatterns = [
     path("inserir/", home_reserva),
+    path("cancelar/<uuid:uuid>", cancel_reserve, name="cancel_reserve"),
 ]
