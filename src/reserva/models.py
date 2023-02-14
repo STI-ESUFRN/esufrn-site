9ef500a849881800058895ecce75430006aa2fe4
Suporte <suporteeen@gmail.com>
2023-02-14 01:36:55 -0300

    fixing unique uuid constraint in reserve model

--- src/reserva/models.py
+++ src/reserva/models.py
@@ -140,7 +140,9 @@ class Reserve(TimeStampedModel, SoftDeletableModel):
     admin_created = models.BooleanField(
         verbose_name="Criado pela administração", default=False
     )
-    uuid = models.CharField(max_length=100, blank=True, unique=True, default=uuid.uuid4)
+    uuid = models.CharField(
+        max_length=100, null=True, blank=True, unique=True, default=uuid.uuid4
+    )
 
     objects = ReserveManager()
 
