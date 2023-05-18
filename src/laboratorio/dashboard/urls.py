3cbb9bb1be8a6a85cf2f1924b912373f29d161ee
Felipe Sena <felipesena.m@gmail.com>
2023-05-18 00:45:09 -0300

    draft: first commit

--- src/laboratorio/dashboard/urls.py
+++ src/laboratorio/dashboard/urls.py
@@ -6,6 +6,7 @@ from laboratorio.dashboard.views import (
     materials_consumable_list_view,
     materials_permanent_list_view,
     permanent_materials_view,
+    report_view,
     update_consumable_view,
     update_permanent_view,
 )
@@ -15,6 +16,7 @@ urlpatterns = [
     path("consumivel/", consumable_materials_view, name="consumable_dashboard"),
     path("consumivel/<int:pk>/", update_consumable_view, name="update_consumable"),
     path("consumivel/relacao/", materials_consumable_list_view, name="consumable_list"),
+    path("consumivel/relatorio/<int:year>", report_view, name="consumable_report"),
     path("permanente/", permanent_materials_view, name="permanent_dashboard"),
     path("permanente/<int:pk>/", update_permanent_view, name="update_permanent"),
     path("permanente/relacao/", materials_permanent_list_view, name="permanent_list"),
--- src/laboratorio/dashboard/views.py
+++ src/laboratorio/dashboard/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import ExpressionWrapper, F, Sum, fields
 from django.http import Http404
 from django.shortcuts import redirect, render
 
@@ -81,4 +82,61 @@ def materials_permanent_list_view(request):
     return render(
         request,
         "laboratorio/dashboard/permanente/relacao.html",
+    )
+
+
+@login_required(login_url="/dashboard/login")
+@allowed_users(allowed_roles=material_roles)
+def report_view(request, year):
+    materials = Consumable.objects.all()
+
+    months = [
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julho",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro",
+    ]
+
+    report = []
+    for material in materials:
+        material_report = {}
+        material_report["name"] = material.name
+        material_report["data"] = []
+
+        for month in range(1, 13):
+            movements = material.history.filter(
+                created__year=year,
+                created__month=month,
+            ).annotate(
+                diff=ExpressionWrapper(
+                    F("quantity") - F("prev_quantity"),
+                    output_field=fields.IntegerField(),
+                ),
+            )
+
+            month_report = {}
+            month_report["month"] = months[month - 1]
+            month_report["highs"] = movements.filter(diff__gt=0).aggregate(
+                total_highs=Sum("diff"),
+            )["total_highs"]
+            month_report["lows"] = movements.filter(diff__lt=0).aggregate(
+                total_lows=Sum("diff"),
+            )["total_lows"]
+
+            material_report["data"].append(month_report)
+
+        report.append(material_report)
+
+    return render(
+        request,
+        "laboratorio/dashboard/consumivel/report.html",
+        {"report": report, "months": months},
     )
