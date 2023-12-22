753826e76c3b013d2b1ea3c94d4bd94d51d46f74
MarconeRafael <marconerafael6@gmail.com>
2023-12-22 08:28:17 -0300

    consertando dashboard chamado

---
+++ src/chamado/static/assets/chamado/dashboard/js/badge.js
@@ -0,0 +1,17 @@
+var count = 0;
+function refreshBadge(ring = true) {
+    $.get("/api/tickets/?status=P", (response) => {
+        if (count != response.count) {
+            count = response.count;
+            $("#badgeChamados").text(count > 0 ? count : "");
+            if (ring) {
+                playAudio();
+            }
+        }
+    });
+}
+
+refreshBadge(false);
+setInterval(() => {
+    refreshBadge();
+}, 10000);
