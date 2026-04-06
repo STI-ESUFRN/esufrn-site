321f4f4baa9618f7a1e3ca46f6ef59789600bc43
kauerbcs <kauereboucas14@gmail.com>
2026-04-06 13:34:22 -0300

    update: ocultando url inserir reserva

--- src/reserva/urls.py
+++ src/reserva/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from reserva.views import calendar_view, cancel_reserve, home_reserva
 
 urlpatterns = [
-    path("inserir/", home_reserva),
+    # path("inserir/", home_reserva), -- deixar oculto por enquanto
     path("calendario/", calendar_view),
     path("cancelar/<uuid:uuid>", cancel_reserve, name="cancel_reserve"),
 ]
